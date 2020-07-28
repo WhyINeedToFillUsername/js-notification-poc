d247f421349ffef4835a2550323492cee7d44dae
akarola <akarola@idc.com>
2020-07-28 21:22:04 +0200

    push notifs - backend

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
+var subscriptionRouter = require('./routes/subscription');
 
 var app = express();
 
@@ -17,8 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
+app.use('/subscription', subscriptionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ services/subscriptionHandler.js
@@ -0,0 +1,47 @@
+const crypto = require("crypto");
+const webpush = require("web-push");
+
+const subscriptions = {}; // should be stored in DB
+
+const vapidKeys = {
+    privateKey: "L2DRgvoFI_iEIybr7i813r7E2IQXIX0bvca2RrbN93A",
+    publicKey: "BOFfYKYT61h76IF5vlPXGnz7DyGFvMT4FkOwVelL_L6_n7Jp3jdIfip1AqCD8gSzwdHV8l-FEOQ6vK7pfPRFZgQ"
+};
+
+webpush.setVapidDetails("mailto:example@yourdomain.org", vapidKeys.publicKey, vapidKeys.privateKey);
+
+function createHash(input) {
+    const md5sum = crypto.createHash("md5");
+    md5sum.update(Buffer.from(input));
+    return md5sum.digest("hex");
+}
+
+function handlePushNotificationSubscription(req, res) {
+    const subscriptionRequest = req.body;
+    const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
+    subscriptions[susbscriptionId] = subscriptionRequest;
+    res.status(201).json({id: susbscriptionId});
+}
+
+function sendPushNotification(req, res) {
+    const subscriptionId = req.params.id;
+    const pushSubscription = subscriptions[subscriptionId];
+    webpush
+        .sendNotification(
+            pushSubscription,
+            JSON.stringify({
+                title: "New Product Available ",
+                text: "HEY! Take a look at this brand new t-shirt!",
+                image: "/images/fit.png",
+                tag: "new-product",
+                url: "/result.html"
+            })
+        )
+        .catch(err => {
+            console.log(err);
+        });
+
+    res.status(202).json({});
+}
+
+module.exports = {handlePushNotificationSubscription, sendPushNotification};
